5ff4fd1c0c213cc1c8e6717af8c786fdf57e0d21
szdzld <160676609@qq.com>
2025-07-25 09:44:34 +0800

    init

--- JMOT/__init__.py
+++ JMOT/__init__.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 import JMOT.connect, JMOT.control, JMOT.craft
 __all__ = ['connect', 'control', 'craft']
-=======
-import connect,  control
-__all__ = ['connect', 'control']
->>>>>>> 736a7f09762a0a945d173d0f4d1bf22c012e01c8
